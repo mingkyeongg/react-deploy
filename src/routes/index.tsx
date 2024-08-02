4c6b26df16aac384fe817a8c8d52f70b534f5e3b
mingkyeongg <ming0820@naver.com>
2024-08-02 10:51:52 +0900

    fix: BrouserRouter에 basename 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 
 import { Layout } from '@/components/features/Layout';
 import { CategoryPage } from '@/pages/Category';
@@ -7,64 +7,32 @@ import { HomePage } from '@/pages/Home';
 import { LoginPage } from '@/pages/Login';
 import { MyAccountPage } from '@/pages/MyAccount';
 import { OrderPage } from '@/pages/Order';
+import KakaoCallbackPage from '@/pages/Login/KakaoCallbackPage';
 
 import { PrivateRoute } from './components/PrivateRoute';
 import { RouterPath } from './path';
-import KakaoCallbackPage from '@/pages/Login/KakaoCallbackPage';
 
-const router = createBrowserRouter([
-  {
-    path: RouterPath.root,
-    element: <Layout />,
-    children: [
-      {
-        path: RouterPath.home,
-        element: <HomePage />,
-      },
-      {
-        path: RouterPath.category,
-        element: <CategoryPage />,
-      },
-      {
-        path: RouterPath.productsDetail,
-        element: <GoodsDetailPage />,
-      },
-      {
-        path: RouterPath.myAccount,
-        element: <PrivateRoute />,
-        children: [
-          {
-            path: RouterPath.myAccount,
-            element: <MyAccountPage />,
-          },
-        ],
-      },
-      {
-        path: RouterPath.order,
-        element: <PrivateRoute />,
-        children: [
-          {
-            path: RouterPath.order,
-            element: <OrderPage />,
-          },
-        ],
-      },
-      {
-        path: RouterPath.notFound,
-        element: <Navigate to={RouterPath.home} />,
-      },
-    ],
-  },
-  {
-    path: RouterPath.login,
-    element: <LoginPage />,
-  },
-  {
-    path: RouterPath.redirect,
-    element: <KakaoCallbackPage />,
-  },
-]);
-
-export const Routes = () => {
-  return <RouterProvider router={router} />;
+const AppRoutes = () => {
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path={RouterPath.root} element={<Layout />}>
+          <Route path={RouterPath.home} element={<HomePage />} />
+          <Route path={RouterPath.category} element={<CategoryPage />} />
+          <Route path={RouterPath.productsDetail} element={<GoodsDetailPage />} />
+          <Route path={RouterPath.myAccount} element={<PrivateRoute />}>
+            <Route path={RouterPath.myAccount} element={<MyAccountPage />} />
+          </Route>
+          <Route path={RouterPath.order} element={<PrivateRoute />}>
+            <Route path={RouterPath.order} element={<OrderPage />} />
+          </Route>
+          <Route path={RouterPath.notFound} element={<Navigate to={RouterPath.home} />} />
+        </Route>
+        <Route path={RouterPath.login} element={<LoginPage />} />
+        <Route path={RouterPath.redirect} element={<KakaoCallbackPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
+
+export default AppRoutes;
